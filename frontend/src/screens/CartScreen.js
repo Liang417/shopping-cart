e70a0842679545fb22a5d2e690c777deb376b864
Liang <72845312+Liang417@users.noreply.github.com>
2022-07-05 12:13:21 +0800

    temp

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,30 @@
 import "./CartScreen.css";
 
+//Component
+import CartItem from "../components/CartItem";
+
 const CartScreen = () => {
-  return <div className="cartscreen">Cart Screen</div>;
+  return (
+    <div className="cartscreen">
+      <div className="cartscreen__left">
+        <h2>Shopping Cart</h2>
+
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
+      </div>
+      <div className="cartscreen__right">
+        <div className="cartscreen__info">
+          <p>總共: (0) 件物品</p>
+          <p>$10</p>
+        </div>
+        <div>
+          <button>去結帳</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartScreen;
